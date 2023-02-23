58e608ebedc892b130ecd8caa46f0759cab7e074
tjdwns4537 <tjdwns4537@naver.com>
2023-02-24 01:16:21 +0900

    feat: project Initialize, MySQL Setting

---
+++ src/main/java/com/example/databasestudy/DatabaseStudyApplication.java
@@ -0,0 +1,12 @@
+package com.example.databasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseStudyApplication.class, args);
+    }
+}
